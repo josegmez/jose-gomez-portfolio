7de03d61b52d3bb511d61f499f025d87b6aa5515
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-09-22 22:43:28 -0500

    refactor: :lipstick: Change global font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const Inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
   weight: "100 900",
 });
 
@@ -24,12 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="es">
+      <body className={`${Inter.variable} antialiased`}>{children}</body>
     </html>
   );
 }
