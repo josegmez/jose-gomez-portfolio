b33e9326b42f5ca497af23dc11f9a87874a6faa5
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-09-22 23:29:19 -0500

    feat: :sparkles: Add component to display a list of skills.

---
+++ src/app/components/atoms/skills-label.tsx
@@ -0,0 +1,25 @@
+import { IconLayersIntersect } from "@tabler/icons-react";
+
+interface SkillsLabelProps {
+  skills: string[];
+}
+
+/**
+ * This component is a custom label that can be used to display a list of skills.
+ * It accepts an array of skills, which will be displayed as a label.
+ *
+ * @param {string[]} skills - The list of skills to display.
+ * @returns {React.ReactNode} The skills label.
+ */
+const SkillsLabel = ({ skills }: SkillsLabelProps) => {
+  const labelString = skills.join(", ");
+
+  return (
+    <div className="flex items-center gap-4 py-1 px-2 rounded-full">
+      <IconLayersIntersect size={18} className="text-primary" />
+      <p>{labelString}</p>
+    </div>
+  );
+};
+
+export default SkillsLabel;
