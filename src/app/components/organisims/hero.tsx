e2a85499f6bf6ad8b07e751d31855412244480aa
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-09-30 14:18:40 -0500

    feat: :sparkles: Add HeroSection component to display the hero section of the page

---
+++ src/app/components/organisims/hero.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import HeroContent from "../molecules/hero-content";
+
+/**
+ * This organism represents the hero section of the page.
+ * Shows an image and the hero content, with a brief description of the developer.
+ *
+ * @returns {JSX.Element} - The component to be rendered
+ */
+const HeroSection = () => {
+  return (
+    <section className="flex justify-between bg-white w-full">
+      <HeroContent className="mt-24 mb-16 ml-16" />
+      <Image
+        src="/images/personal-image.png"
+        alt="Personal image"
+        width={350}
+        height={200}
+      />
+    </section>
+  );
+};
+
+export default HeroSection;
