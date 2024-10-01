ac2f6f07aef151ccbcc62d12b9ee75ed436da504
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-10-01 00:43:23 -0500

    feat: :sparkles: Add all orgasims to main portfolio page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,38 @@
+import HeroSection from "@organisms/hero";
+import KnowlageSection from "@organisms/knowlage";
+import EducationSection from "@organisms/education";
+import ProjectsSection from "@organisms/projects";
+import LeftMenu from "@organisms/left-menu";
+import RightMenu from "@organisms/right-menu";
+import Footer from "@organisms/footer";
+
+/**
+ * This is the landing page of the application.
+ * It shows the hero section, knowlage section, education section, projects section and footer.
+ *
+ * This page is composed by the following organisms:
+ * - HeroSection
+ * - KnowlageSection
+ * - EducationSection
+ * - ProjectsSection
+ * - Footer
+ * - LeftMenu
+ * - RightMenu
+ *
+ * @returns {JSX.Element} - The component to be rendered
+ */
 export default function Home() {
   return (
-    <main>
+    <main className="grid grid-cols-[3fr,10fr,1fr] gap-8 min-h-screen">
+      <LeftMenu />
+      <div className="flex flex-col items-center gap-12">
+        <HeroSection />
+        <KnowlageSection />
+        <EducationSection />
+        <ProjectsSection />
+        <Footer />
+      </div>
+      <RightMenu />
     </main>
   );
 }
